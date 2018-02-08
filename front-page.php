41bc4f21681ed4528d21dd1e1677455524d01c6f
nazarpelekh <nazar_peleh@meta.ua>
2018-02-08 18:02:57 +0200

    home page 06.02

--- front-page.php
+++ front-page.php
@@ -1,157 +1,113 @@
 <?php get_header(); ?>
 
 <section class="page-board front-board" style='background-image: url("<?php the_field('top_b_img'); ?>");'>
-    <a href="#" class="logo-board"><img src="<?php the_field('top_b_logo'); ?>" alt=""></a>
+    <a href="#" class="logo-board"><img src="<?php the_field('logo','option'); ?>" alt=""></a>
     <div class="board-text"><?php the_field('top_b_caption'); ?></div>
     <div class="scroll-button scroll-button--down"></div>
 </section>
-            
-            <?php
-                $args = array(
-                    'post_type' => 'blog',
-                    //'orderby' => 'name',
-                    'order' => 'ASC',
-                    'hide_empty'  => 0,
-                    'taxonomy' => 'blog_artical_category',
-                    'pad_counts' => false
-                );
-                $categories = get_categories($args);
-            ?>
-
-            <?php foreach ($categories as $category) {
-                $args = array(
-                    'post_type' => 'blog',
-                    'blog_artical_category' => $category->name,
-                );
-                $loop = new WP_Query($args);
-                while ($loop->have_posts()) {
-                    $loop->the_post();
-                    ?>
-                        <?php echo $category->name; ?><br>
-                        <?php the_title(); ?> <br>
-                        <?php echo get_the_post_thumbnail_url($post,'full' ) ?><br>
-                        <?php echo get_the_date('d F o'); ?><br>
-                        <?php the_excerpt(); ?><br>
-                <?php   }
-            } ?>
-            <?php foreach ($categories as $category) { ?>
-                <a href="<?php echo get_term_link($category->term_id, $category->taxonomy); ?>"><?php echo $category->name; ?></a>
-            <?php } ?>
-            <?php wp_reset_query(); ?>
-
-
-
-
-
-
-
-
 
 <a id="front-anchor"></a>
 <section class="news-front-section">
 
-	<?php
-	$args = array(
-		'post_type'   => 'blog',
-		'post_status' => 'publish',
-	);
-
-	$testimonials = new WP_Query( $args );
-	if( $testimonials->have_posts() ) :
-		?>
-        <ul>
-			<?php
-			while( $testimonials->have_posts() ) :
-				$testimonials->the_post();
-				?>
-                <li><?php printf( '%1$s - %2$s', get_the_title(), get_the_content() );  ?></li>
-				<?php
-			endwhile;
-			wp_reset_postdata();
-			?>
-        </ul>
-		<?php
-	else :
-		esc_html_e( 'No testimonials in the diving taxonomy!', 'text-domain' );
-	endif;
-	?>
-
-	<?php if (have_posts()) : $p = 0; while (have_posts()) : the_post(); ?>
-        <div class="posts">
-		<?php if($p == 0 && $paged == 1) { ?>
-            <a href="<?php the_permalink(); ?>" class="first_post post">
-                <div class="post__thumb"><?php the_post_thumbnail('single');?></div>
-                <div class="post__content">
-                    <h4><?php the_title(); ?></h4>
-                    <time datetime="<?php echo get_the_date('M. j. Y'); ?>"><?php echo get_the_date(); ?></time>
-                    <!--                                <p>--><?php //echo wp_trim_words(get_the_content(), 50, ''); ?><!--...<i class="more icon-right-btn"> Read More</i></p>-->
-                    <p><?php echo substr( strip_tags( get_the_content() ), 0, 146 );; ?>...</p>
-                    <i class="more icon-right-btn"> Read More</i>
-                </div>
-            </a>
-		<?php } else { ?>
-            <div class="post">
-                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('prev_index');?></a>
-                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                <time datetime="<?php echo get_the_date(); ?>"><?php echo get_the_date('M. j. Y'); ?></time>
-                <p><?php echo wp_trim_words(get_the_content(), 35, ''); ?><a href="<?php the_permalink(); ?>" class="more icon-right-btn"> Read More</a></p>
-            </div>
-		<?php } ?>
-		<?php $p++; endwhile; ?>
-        </div>
-	<?php endif; ?>
-
     <div class="container">
-        <div class="news-block news-block--large clearfix">
-            <div class="news-img block-left"><img src="<?php echo get_template_directory_uri(); ?>/img/img-01.jpg" alt=""></div>
-            <div class="news-text block-left">
-                <div class="attribute-news clearfix">
-                    <div class="attr-name block-left">DOSSIER</div>
-                    <div class="news-date block-right">10 juin 2017</div>
-                </div>
-                <div class="news-title">Assouplissement du droit de succession</div>
-                <div class="news-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus posuere eros, eget eleifend tortor hendrerit eget. Proin orci nulla, posuere vel ante eu, suscipit cursus augue. Sed consectetur dolor eget molestie pharetra. Curabitur suscipit metus sollicitudin, efficitur mi semper, dictum purus. Phasellus dictum felis purus, nec consequat ipsum volutpat eu. Proin non elementum lectus. Nam porttitor dignissim </div>
-                <a href="#" class="news-link">Lire la suite</a>
-            </div>
-        </div>
+
+	    <?php
+	    $args = array(
+		    'post_type' => 'blog',
+		    //'orderby' => 'name',
+		    'order' => 'ASC',
+		    'hide_empty'  => 0,
+		    'taxonomy' => 'blog_artical_category',
+		    'pad_counts' => false,
+		    'post_per_page' => 1
+	    );
+	    $categories = get_categories($args);
+	    ?>
+
+	    <?php foreach ($categories as $category) {
+		    $args = array(
+			    'post_type' => 'blog',
+			    'blog_artical_category' => $category->name,
+		    );
+		    $loop = new WP_Query($args);
+		    static $big_post = 0;
+		    while ($loop->have_posts()) { $loop->the_post(); ?>
+
+			    <?php if($big_post == 0) { ?>
+                    <div class="news-block news-block--large clearfix">
+                        <div class="news-img block-left"><img src="<?php echo get_the_post_thumbnail_url($post,'full' ) ?>" alt=""></div>
+                        <div class="news-text block-left">
+                            <div class="attribute-news clearfix">
+                                <div class="attr-name block-left"><?php echo $category->name; ?></div>
+                                <div class="news-date block-right"><?php echo get_the_date('d F o'); ?></div>
+                            </div>
+                            <div class="news-title"><?php the_title(); ?></div>
+                            <div class="news-description"><?php echo wp_trim_words( get_the_content(), 40, '' ); ?></div>
+                            <a href="<?php the_permalink(); ?>" class="news-link">Lire la suite</a>
+                        </div>
+                    </div>
+			    <?php } else { ?>
+
+			    <?php }
+			    $big_post++;
+		    }
+	    } ?>
+	    <?php wp_reset_query(); ?>
+
         <div class="news-list-block clearfix">
-          <div class="news-block block-left">
-            <div class="news-img"><img src="<?php echo get_template_directory_uri(); ?>/img/img-02.jpg" alt=""></div>
-            <div class="news-text">
-              <div class="attribute-news clearfix">
-                <div class="attr-name block-left">ACTUALITÉ</div>
-                <div class="news-date block-right">10 juin 2017</div>
-              </div>
-              <div class="news-title">Testament : les erreurs à ne pas commettre</div>
-              <a href="#" class="news-link">Lire la suite</a>
-            </div>
-          </div>
-          <div class="news-block block-left">
-            <div class="news-img"><img src="<?php echo get_template_directory_uri(); ?>/img/img-03.jpg" alt=""></div>
-            <div class="news-text">
-              <div class="attribute-news clearfix">
-                <div class="attr-name block-left">DOSSIER</div>
-                <div class="news-date block-right">10 juin 2017</div>
-              </div>
-              <div class="news-title">Comment pérenniser sa générosité</div>
-              <a href="#" class="news-link">Lire la suite</a>
-            </div>
-          </div>
-          <div class="news-block block-left">
-            <div class="news-img"><img src="<?php echo get_template_directory_uri(); ?>/img/img-03.jpg" alt=""></div>
-            <div class="news-text">
-              <div class="attribute-news clearfix">
-                <div class="attr-name block-left">ACTUALITÉ</div>
-                <div class="news-date block-right">10 juin 2017</div>
-              </div>
-              <div class="news-title">Le PACS : Moins protecteur que le mariage en cas de décès</div>
-              <a href="#" class="news-link">Lire la suite</a>
-            </div>
-          </div>
+
+	        <?php
+	        $args = array(
+		        'post_type' => 'blog',
+		        //'orderby' => 'name',
+		        'order' => 'ASC',
+		        'hide_empty'  => 0,
+		        'taxonomy' => 'blog_artical_category',
+		        'pad_counts' => false,
+		        'post_per_page' => 4
+	        );
+	        $categories = get_categories($args);
+	        ?>
+
+	        <?php foreach ($categories as $category) {
+		        $args = array(
+			        'post_type' => 'blog',
+			        'blog_artical_category' => $category->name,
+		        );
+		        $loop = new WP_Query($args);
+		        static $small = 0;
+		        while ($loop->have_posts()) { $loop->the_post(); ?>
+
+			        <?php if($small == 0) { ?>
+
+			        <?php } else { ?>
+                        <div class="news-block block-left">
+                            <div class="news-img"><img src="<?php echo get_the_post_thumbnail_url($post,'full' ) ?>" alt=""></div>
+                            <div class="news-text">
+                                <div class="attribute-news clearfix">
+                                    <div class="attr-name block-left"><?php echo $category->name; ?></div>
+                                    <div class="news-date block-right"><?php echo get_the_date('d F o'); ?></div>
+                                </div>
+                                <div class="news-title"><?php the_title(); ?></div>
+                                <a href="<?php the_permalink(); ?>" class="news-link">Lire la suite</a>
+                            </div>
+                        </div>
+			        <?php }
+			        $small++;
+		        }
+	        } ?>
+	        <?php wp_reset_query(); ?>
+
         </div>
         <div class="news-front-links text-center">
-            <a href="#" class="button"><span>TOUS NOS DOSSIERS ></span></a>
-            <a href="#" class="button"><span>TOUtes les actualités ></span></a>
+	        <?php foreach ($categories as $category) { ?>
+		        <?php if($category->name == 'ACTUALITÉ') { ?>
+                    <a href="<?php echo get_term_link($category->term_id, $category->taxonomy); ?>" class="button"><span>TOUtes les actualités ></span></a>
+		        <?php } ?>
+		        <?php if($category->name == 'DOSSIER') { ?>
+                    <a href="<?php echo get_term_link($category->term_id, $category->taxonomy); ?>" class="button"><span>TOUS NOS DOSSIERS ></span></a>
+		        <?php } ?>
+	        <?php } ?>
         </div>
     </div>
 </section>
