089e55399a585b9c4c24dca15c4def3600d027e0
DESKTOP-6Q5LF2L\Alex <sifters.imonkeys@gmail.com>
2018-02-12 12:31:44 +0200

    12.02.2018 start

---
+++ archive-faq.php
@@ -0,0 +1,128 @@
+<?php get_header(); ?>
+
+<div class="return-block"><a href="#">RETOUR</a></div>
+
+<section class="page-title-section">
+    <div class="container">
+        <div class="page-title">FOIRE AUX QUESTIONS</div>
+        <h3>Les questions fréquemment posées</h3>
+    </div>
+</section>
+<section class="breadcrumbs-section">
+    <div class="container">
+        <ul class="breadcrumbs clearfix">
+            <li><a href="#">Accueil</a></li>
+            <li>Foire aux questions</li>
+        </ul>
+    </div>
+</section>
+
+<section class="page-content">
+    <div class="container">
+
+        <div class="page-text-content text-center">
+            <strong>Lorem ipsum dolor sit amet,</strong> <a href="#">consectetur adipiscing elit</a>. Nam iaculis, orci at aliquam vulputate, lorem enim faucibus magna, sit amet molestie ligula sapien eu risus. Suspendisse eu nisi in lorem rutrum suscipit non vel lacus. Praesent placerat, dui eget rhoncus placerat, nisi neque viverra odio, sit amet consectetur ipsum orci ac sapien. Praesent congue diam erat, a laoreet augue convallis ullamcorper. Nulla a sem lectus. Nunc scelerisque lectus quis diam facilisis, nec faucibus arcu hendrerit. Fusce risus risus, elementum a rutrum id, lacinia et eros. Vivamus efficitur quis lectus in pulvinar. Sed tincidunt luctus sapien nec dignissim. Mauris velit nisi, faucibus eu nibh vel, porttitor venenatis urna. Maecenas feugiat felis non mi porttitor pulvinar.
+        </div>
+
+        <ul class="faq-page-list">
+            <li>
+                <div class="question-block" data-collapse="true" data-target="q01">
+                    <span>Questions sur le legs</span>
+                </div>
+                <div id="q01" class="answer-block">
+                    <div class="innter-wrap">
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="question-block" data-collapse="true" data-target="q02">
+                    <span>Questions sur le legs</span>
+                </div>
+                <div id="q02" class="answer-block">
+                    <div class="innter-wrap">
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="question-block" data-collapse="true" data-target="q03">
+                    <span>Questions sur le legs</span>
+                </div>
+                <div id="q03" class="answer-block">
+                    <div class="innter-wrap">
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="question-block" data-collapse="true" data-target="q04">
+                    <span>Questions sur le legs</span>
+                </div>
+                <div id="q04" class="answer-block">
+                    <div class="innter-wrap">
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod dolor set amet tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                        <p>Lorem ipsum et doloris doloris eiusmod tempor doloris eiusmod tempor ?</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+</section>
+
+<div class="content-link-block">
+  <div class="container">
+    <ul class="content-link-list clearfix">
+      <li class="block-left width-33 text-center">
+        <div class="wrapper">
+          <div class="icon-block">
+            <div class="icon icon--list"></div>
+          </div>
+          <strong class="description">Découvrir notre expertise sur le legs et la création de fondations au travers de nos dossiers.</strong>
+          <a href="#" class="button"><span>TOUS NOS DOSSIERS ></span></a>
+        </div>
+      </li>
+      <li class="block-left width-33 text-center">
+        <div class="wrapper">
+          <div class="icon-block">
+            <div class="icon icon--letter"></div>
+          </div>
+          <strong class="description">Si vous ne trouvez pas la réponse à votre question, contactez notre équipe d'expert.</strong>
+          <a href="#" class="button"><span>contactez-nous ></span></a>
+        </div>
+      </li>
+      <li class="block-left width-33 text-center">
+        <div class="wrapper">
+          <div class="icon-block">
+            <div class="icon icon--book"></div>
+          </div>
+          <strong class="description">Un terme, un mot incompris, consultez notre glossaire.</strong>
+          <a href="#" class="button"><span>notre glossaire ></span></a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class="button-to-top">
+  <span class="fa fa-angle-up"></span>
+  Haut <br/>de la page
+</div>
+
+<?php get_footer(); ?>
