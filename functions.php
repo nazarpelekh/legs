d8029719e90dc227ec061ee6df2fdd7f7d123fcb
nazarpelekh <nazar_peleh@meta.ua>
2018-02-13 19:09:54 +0200

    Single

--- functions.php
+++ functions.php
@@ -793,3 +793,65 @@ function my_taxonomies_faq_artical() {
 }
 add_action( 'init', 'my_taxonomies_faq_artical', 0 );
 
+//====================================================================================================================
+//==========================================assurance-vie CUSTOM POST TYPE======================================================
+//====================================================================================================================
+
+function add_assurance_posts(){
+    register_post_type(
+        'assurance',
+        array(
+            'labels'        => array(
+                'name'                  => 'Assurance-via',
+                'singular_name'         => 'Assurance item',
+                'add_new'               => 'Add new',
+                'add_new_item'          => 'Add new item',
+                'edit'                  => 'Edit',
+                'edit_item'             => 'Edit item',
+                'new_item'              => 'New item',
+                'view'                  => 'View',
+                'view_item'             => 'View item',
+                'search_items'          => 'Search item',
+                'not_found'             => 'Not found',
+                'not_found_in_trash'    => 'Not find in trash',
+            ),
+            'public'        => true,
+            'hierarchical'  => true,
+            'has_archive'   => true,
+            'menu_icon'     => 'dashicons-admin-multisite',
+            'supports'      => array(
+                'title',
+                'editor',
+                'thumbnail',
+                //'post-formats',
+                'assurance_artical_category'
+            ),
+            'can_export' => true,
+        )
+    );
+}
+add_action('init','add_assurance_posts');
+
+function my_taxonomies_assurance_artical() {
+    $labels = array(
+        'name'              => _x( 'Category assurance', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Singular name', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search items' ),
+        'all_items'         => __( 'All item' ),
+        'parent_item'       => __( 'Parent item' ),
+        'parent_item_colon' => __( 'Parent item colon' ),
+        'edit_item'         => __( 'Edit item' ),
+        'update_item'       => __( 'Update item' ),
+        'add_new_item'      => __( 'Add new item' ),
+        'new_item_name'     => __( 'New item name' ),
+        'menu_name'         => __( 'Assurance-via Category' ),
+    );
+    $args = array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'show_ui'           => true,
+        'show_admin_column' => true
+    );
+    register_taxonomy( 'assurance_artical_category', 'assurance', $args );
+}
+add_action( 'init', 'my_taxonomies_assurance_artical', 0 );
